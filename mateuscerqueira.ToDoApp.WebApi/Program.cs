2cefe422d41a5bdbede654fcf9393b2a7943a0f1
Mateus Cerqueira <mateusjesus2309@gmail.com>
2025-09-14 13:55:06 +0100

    feat(deploy): Adicionado Render.yaml para inicio do deploy.

--- mateuscerqueira.ToDoApp.WebApi/Program.cs
+++ mateuscerqueira.ToDoApp.WebApi/Program.cs
@@ -2,6 +2,36 @@ using mateuscerqueira.ToDoApp.IoC;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
+builder.WebHost.UseUrls($"http://*:{port}");
+
+if (builder.Environment.IsProduction())
+{
+    builder.Services.AddCors(options =>
+    {
+        options.AddPolicy("AllowProduction",
+            policy =>
+            {
+                policy.WithOrigins("https://seu-frontend.vercel.app")
+                      .AllowAnyHeader()
+                      .AllowAnyMethod();
+            });
+    });
+}
+else
+{
+    builder.Services.AddCors(options =>
+    {
+        options.AddPolicy("AllowAll",
+            policy =>
+            {
+                policy.AllowAnyOrigin()
+                      .AllowAnyHeader()
+                      .AllowAnyMethod();
+            });
+    });
+}
+
 builder.Services.AddIocServices(builder.Configuration);
 
 builder.Services.AddControllers();
@@ -9,6 +39,17 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngularApp",
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:4200", "https://localhost:4200")
+                  .AllowAnyHeader()
+                  .AllowAnyMethod()
+                  .AllowCredentials();
+        });
+});
 
 var app = builder.Build();
 
@@ -20,6 +61,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("AllowAngularApp");
 
 app.UseAuthorization();
 
